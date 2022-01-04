c5cea4e5b826c9846063ffe64aebb3ab0086fb67
Bagas Dwi <shirrogannie@gmail.com>
2022-01-04 19:36:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("close").style.marginTop = "0"
+}
+function closeNav() {
+    document.getElementById("close").style.marginTop = "350%"
+}
+function openAlert() {
+    document.getElementById("alert").style.marginTop = "300px",
+    document.getElementById("close").style.marginTop = "350%"
+}
+function closeAlert() {
+    document.getElementById("alert").style.marginTop = "300%"
+}
